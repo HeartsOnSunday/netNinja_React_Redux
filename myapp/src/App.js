ec3a085fe3f1890d6900665f970b17c63189c771
HeartsOnSunday <leahgdg@hotmail.com>
2018-08-21 21:02:17 -0400

    added dynamic props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import Ninjas from "./Ninjas";
 class App extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="App">
         <h1>My first React App</h1>
         <p>Go screw yourself!</p>
-        <Ninjas />
+        <Ninjas name="Ryu" age="25" belt="Black" />
+        <Ninjas name="loser" age="33" belt="None" />
       </div>
     );
   }
